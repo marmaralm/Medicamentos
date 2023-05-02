5973c7158900bbadce703ce2f5ff6c6fa227697a
1704298 <marmaralm@portugalmail.pt>
2023-05-02 10:43:52 +0100

    Initial commit

---
+++ app/src/main/java/com/example/medicamentos/BDMEd.kt
@@ -0,0 +1,22 @@
+package com.example.medicamentos
+
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+private const val NOME_BASE_DADOS
+class BdMedicamentosOpenHelper(
+    context: Context?,
+    name: String?,
+    factory: SQLiteDatabase.CursorFactory?,
+    version: Int
+) : SQLiteOpenHelper(context, name, factory, version) {
+    override fun onCreate(p0: SQLiteDatabase?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+        TODO("Not yet implemented")
+    }
+}
