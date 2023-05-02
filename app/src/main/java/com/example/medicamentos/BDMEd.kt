5e5cb0bae2560378eb363103c31f7d685b8f683d
1704298 <marmaralm@portugalmail.pt>
2023-05-02 10:45:48 +0100

    definir o nome da base de dados

--- app/src/main/java/com/example/medicamentos/BDMEd.kt
+++ app/src/main/java/com/example/medicamentos/BDMEd.kt
@@ -5,13 +5,15 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-private const val NOME_BASE_DADOS
+private const val NOME_BASE_DADOS = "medicamentod.db"
+private const val VERSAO_BASE_DADOS = 1
+
 class BdMedicamentosOpenHelper(
     context: Context?,
     name: String?,
     factory: SQLiteDatabase.CursorFactory?,
     version: Int
-) : SQLiteOpenHelper(context, name, factory, version) {
+) : SQLiteOpenHelper(context, NOME_BASE_DADOS, null, VERSAO_BASE_DADOS) {
     override fun onCreate(p0: SQLiteDatabase?) {
         TODO("Not yet implemented")
     }
